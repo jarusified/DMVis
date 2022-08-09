9d9162ec6427ba0b3d67882661353c81d4a4bc74
surajk <suraj.kesavan@sambanovasystems.com>
2022-08-08 17:14:29 -0700

    Add the server

---
+++ server/argparser.py
@@ -0,0 +1,64 @@
+import argparse
+from logger import get_logger
+
+LOGGER = get_logger(__name__)
+
+
+class ArgParser:
+    """
+    Argparser class decodes the arguments passed to
+    """
+
+    def __init__(self, args_string):
+
+        assert isinstance(args_string, list)
+
+        # Parse the arguments passed.
+        self.parser = ArgParser._create_parser()
+        self.args = vars(self.parser.parse_args())
+
+        # Verify if only valid things are passed.
+        self._verify_parser()
+        LOGGER.info(f"Arguments verified.")
+
+    def __str__(self):
+        items = ("%s = %r" % (k, v) for k, v in self.__dict__.items())
+        return "<%s: {%s}> \n" % (self.__class__.__name__, ", ".join(items))
+
+    def __repr__(self):
+        return self.__str__()
+
+    # --------------------------------------------------------------------------
+    # Private methods.
+    @staticmethod
+    def _create_parser():
+        """
+        Parse the input arguments.
+        """
+        parser = argparse.ArgumentParser(prefix_chars="--")
+        parser.add_argument('--http', help="Server mode -- Spawns a http server on specified port", type=int, required=False)
+        parser.add_argument('--data_dir', help='Data directory path', type=str, required=True)
+        return parser
+
+    def _verify_parser(self):
+        """
+        Verify the input arguments.
+
+        Raises expections if something is not provided
+        Check if the config file is provided and exists!
+
+        :pargs : argparse.Namespace
+            Arguments passed by the user.
+
+        Returns
+        -------
+        """
+
+        _has_data_dir = self.args["data_dir"] is not None
+
+        if not _has_data_dir:
+            LOGGER.error(f'Option --data_dir not provided.')
+            self.parser.print_help()
+            exit(1)
+        
+        return 
---
+++ server/interfaces/timeline.py
@@ -0,0 +1,84 @@
+import os
+import json
+from logger import get_logger
+from datetime import datetime
+
+LOGGER = get_logger(__name__)
+
+class Timeline():
+    def __init__(self, data_dir):
+        """
+        Initializes a Timeline object.
+        """
+        LOGGER.info(f"{type(self).__name__} interface triggered.")
+        self.experiments = os.listdir(data_dir)
+        self.file_paths = {exp: os.path.join(os.path.abspath(data_dir), exp) for exp in self.experiments}
+        self.timelines = {exp: self.load_timeline(self.file_paths[exp]) for exp in self.experiments}
+
+        LOGGER.info(f"Loaded {len(self.timelines)} timelines.")
+
+    @staticmethod
+    def clean_address(d):
+        return {k: v for k, v in d.items() if k != 'alloc.address'}
+
+    def load_timeline(self, file_path):
+        """
+        Loads a timeline from a JSON file.
+        """
+        with open(file_path, 'r') as f:
+            d = json.load(f)            
+            traceEvents = self.post_process(d["data"]["traceEvents"])
+            
+
+        return traceEvents  
+
+    def sort_by_event_count(self):
+        event_counts_dict = { exp: len(self.timelines[exp]) for exp in self.experiments }
+        return list(dict(sorted(event_counts_dict.items(), key=lambda item: item[1], reverse=False)).keys())
+
+    def get_timeline(self, exp):
+        """
+        Returns a timeline for a given experiment.
+        """
+        return self.timelines[exp] if exp in self.experiments else None
+
+    def post_process(self, events):
+        controlled_events = ["already evaluated", "empty tensor"]
+        indices = [idx for idx, event in enumerate(events) if event["name"] not in controlled_events]
+        non_indices =  [idx for idx, event in enumerate(events) if event["name"] in controlled_events]
+        
+        ret = []
+        event_idx = 0
+        for _idx in range(0, len(indices) - 1, 2):
+            s = events[indices[_idx]]
+            e = events[indices[_idx + 1]]
+            
+            # assert(s["name"] == e["name"])
+            _event = {
+                # "args": s["args"],
+                "content": s["name"],
+                "start": s["ts"],
+                "end": e["ts"],
+                "pid": s["pid"],
+                "tid": s["tid"],
+                "id": event_idx
+            }
+            event_idx += 1
+            
+            ret.append(_event)
+            
+        for _idx in non_indices:
+            _e = events[_idx]
+            _event = {
+                "args": _e["args"],
+                "content": _e["name"],
+                "start": _e["ts"],
+                "end": _e["ts"],
+                "pid": _e["pid"],
+                "tid": _e["tid"],
+                "id": event_idx
+            }
+            event_idx += 1
+            ret.append(_event)
+            
+        return ret
