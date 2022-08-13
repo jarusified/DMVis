79ce5d1f279fc9f7df923cf1f9dc45094baae950
surajk <suraj.kesavan@sambanovasystems.com>
2022-08-12 20:52:43 -0700

    Add group id to the event data

--- server/interfaces/timeline.py
+++ server/interfaces/timeline.py
@@ -1,8 +1,8 @@
 import os
 import json
 from utils.time import format_timestamp
+from utils.general import remap_dict_of_list
 from logger import get_logger
-from datetime import datetime
 
 LOGGER = get_logger(__name__)
 
@@ -13,7 +13,24 @@ class Timeline():
         """
         LOGGER.info(f"{type(self).__name__} interface triggered.")
         self.experiments = os.listdir(data_dir)
+
         self.file_paths = {exp: os.path.join(os.path.abspath(data_dir), exp) for exp in self.experiments}
+
+        self.event_to_groups = {
+            "runtime": ["runtime"],
+            "compile": ["compile"],
+            "tracing": ["tracing", "tracing-start", "tracing-end", "already evaluated", "empty tensor"]
+        }
+        self.group_to_event = remap_dict_of_list(self.event_to_groups)
+        self.point_events = ["already evaluated", "empty tensor"]
+        self.class_names = {
+            "tracing-start": "magenta",
+            "tracing": "magenta",
+            "runtime": "orange",
+            "compile": "red",
+            "tracing-end": "magenta"
+        }
+
         self.timelines = {exp: self.load_timeline(self.file_paths[exp]) for exp in self.experiments}
 
         LOGGER.info(f"Loaded {len(self.timelines)} timelines.")
@@ -46,54 +63,63 @@ class Timeline():
         """
         return self.timelines[exp] if exp in self.experiments else None
 
-    def post_process(self, events):
-        controlled_events = ["already evaluated", "empty tensor"]
-        class_names = {
-            "tracing-start": "magenta",
-            "tracing": "magenta",
-            "runtime": "orange",
-            "compile": "red",
-            "tracing-end": "magenta"
+    def _add_range_events(self, start, end, idx):
+        """
+        Utility to format the range-based events (i.e., with a start and end timestamp). 
+
+        Format details: 
+        """
+        return {
+            "args": start["args"],
+            "name": start["name"],
+            "className": self.class_names[start["name"]],
+            "content": start["name"],
+            "group": start["name"],
+            "end": format_timestamp(end["ts"]),
+            "id": idx,
+            "start": format_timestamp(start["ts"]),
+            "pid": start["pid"],
+            "tid": start["tid"],
+            "group": self.group_to_event[start["name"]]
         }
-        indices = [idx for idx, event in enumerate(events) if event["name"] not in controlled_events]
-        non_indices =  [idx for idx, event in enumerate(events) if event["name"] in controlled_events]
+
+    def _add_point_events(self, event, idx):
+        """
+        Utility to format the point-based events (i.e., with a start timestamp).
+        """
+        return {
+            "args": event["args"],
+            "name": event["name"],
+            "type": "point",
+            # "className": class_names[_e["name"]],
+            "content": event["name"],
+            "id": idx,
+            "pid": event["pid"],
+            "start": format_timestamp(event["ts"]),
+            "tid": event["tid"],
+            "group": self.group_to_event[event["name"]]
+        }
+
+    def post_process(self, events):
+        """
+        
+        """
+        indices = [idx for idx, event in enumerate(events) if event["name"] not in self.point_events]
+        non_indices = [idx for idx, event in enumerate(events) if event["name"] in self.point_events]
         
         ret = []
         event_idx = 0
         for _idx in range(0, len(indices) - 1, 2):
             s = events[indices[_idx]]
             e = events[indices[_idx + 1]]
-            
-            # assert(s["name"] == e["name"])
-            _event = {
-                # "args": s["args"],
-                "name": s["name"],
-                "className": class_names[s["name"]],
-                "content": s["name"],
-                "end": format_timestamp(e["ts"]),
-                "id": event_idx,
-                "start": format_timestamp(s["ts"]),
-                "pid": s["pid"],
-                "tid": s["tid"]
-            }
-            event_idx += 1
-            
+            _event = self._add_range_events(s, e, event_idx)
             ret.append(_event)
-            
+            event_idx += 1
+
         for _idx in non_indices:
             _e = events[_idx]
-            _event = {
-                "args": _e["args"],
-                "name": _e["name"],
-                "type": "point",
-                # "className": class_names[_e["name"]],
-                # "content": _e["name"],
-                "id": event_idx,
-                "pid": _e["pid"],
-                "start": format_timestamp(_e["ts"]),
-                "tid": _e["tid"]
-            }
-            event_idx += 1
+            _event = self._add_point_events(_e, event_idx)
             ret.append(_event)
+            event_idx += 1
             
         return ret
