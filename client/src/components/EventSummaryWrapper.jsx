4f9f0e800b3005c066444b6fb52e2b9548baf6e2
jarusified <jarus3001@gmail.com>
2022-10-01 17:28:07 -0700

    Move to bar chart

--- client/src/components/EventSummaryWrapper.jsx
+++ client/src/components/EventSummaryWrapper.jsx
@@ -1,9 +1,8 @@
-import * as d3 from "d3";
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 import { fetchEventSummary } from "../actions";
-import { COLORS, formatTimestamp } from "../helpers/utils";
+import D3BarGraph from "../ui/d3-bar-graph";
 
 function EventSummaryWrapper() {
 	const dispatch = useDispatch();
@@ -13,11 +12,15 @@ function EventSummaryWrapper() {
 	);
 	const selectedExperiment = useSelector((store) => store.selectedExperiment);
 
-	const margin = { top: 10, right: 20, bottom: 10, left: 20 };
-	const containerWidth = window.innerWidth / 2;
+	const style = {
+		top: 10,
+		right: 20,
+		bottom: 10,
+		left: 20,
+		width: window.innerWidth / 2,
+		height: window.innerHeight / 4
+	};
 	const containerHeight = window.innerHeight / 4;
-	const width = containerWidth - margin.left - margin.right;
-	const height = containerHeight - margin.bottom - margin.top;
 	const containerID = "#event-summary-view";
 
 	useEffect(() => {
@@ -27,91 +30,12 @@ function EventSummaryWrapper() {
 	}, [selectedExperiment]);
 
 	useEffect(() => {
-		d3.select(containerID).selectAll("*").remove();
 		if (
 			currentEventSummary != undefined &&
 			currentEventSummary.length > 0
 		) {
-			const events = currentEventSummary.map((bar) => bar.event);
-			const durations = currentEventSummary.map((bar) => bar.dur);
-
-			let x = d3
-				.scaleBand()
-				.domain(events)
-				.range([0, width])
-				.padding(0.2);
-
-			let y = d3
-				.scaleLinear()
-				.domain([d3.min(durations), d3.max(durations)])
-				.range([height, 0])
-				.nice(5);
-
-			let xAxis = d3
-				.axisBottom()
-				.scale(x)
-				.ticks(10)
-				.tickPadding(-x.bandwidth() / 2)
-				.tickSizeOuter(2)
-				.tickFormat((d) => d);
-
-			let yAxis = d3
-				.axisLeft()
-				.scale(y)
-				.ticks(5)
-				.tickFormat((d) => formatTimestamp(d));
-
-			let svg = d3
-				.select(containerID)
-				.append("svg")
-				.attr("width", width)
-				.attr("height", height)
-				.append("g")
-				.attr(
-					"transform",
-					"translate(" + 2 * margin.left + "," + -margin.bottom + ")"
-				);
-
-			svg.selectAll("bars")
-				.data(currentEventSummary)
-				.enter()
-				.append("rect")
-				.attr("x", (d) => {
-					return x(d.event);
-				})
-				.attr("y", (d) => {
-					return y(d.dur);
-				})
-				.attr("class", (d) => {
-					return d.class_name;
-				})
-				.attr("width", x.bandwidth())
-				.attr("height", (d) => {
-					return height - y(d.dur);
-				})
-				.attr("fill", (d) => {
-					return COLORS[d.class_name];
-				});
-
-			svg.append("g")
-				.attr("class", "x axis")
-				.attr("transform", "translate(0," + height + ")")
-				.call(xAxis)
-				.selectAll("text")
-				.attr("dy", x.bandwidth() / 2)
-				.attr("dx", -3)
-				.style("text-anchor", "end")
-				.attr("transform", "rotate(90)");
-
-			svg.append("g")
-				.attr("class", "y axis")
-				.call(yAxis)
-				.append("text")
-				.attr("transform", "rotate(-90)")
-				.attr("y", 0)
-				.attr("dy", "1em")
-				.style("text-anchor", "end")
-				.text("time (s))");
+			console.log(currentEventSummary);
+			D3BarGraph(containerID, style, currentEventSummary, "event", "dur");
 		}
 	}, [currentEventSummary]);
 	return <div id="event-summary-view"></div>;
---
+++ client/src/ui/d3-bar-graph.jsx
@@ -0,0 +1,91 @@
+import * as d3 from "d3";
+
+import { COLORS, formatTimestamp } from "../helpers/utils";
+
+function D3BarGraph(containerID, style, data, xProp, yProp) {
+	const width = style.width - style.left - style.right;
+	const height = style.height - style.bottom - style.top;
+
+	// Clean up existing elements
+	d3.select(containerID).selectAll("*").remove();
+
+	const xData = data.map((d) => d[xProp]);
+	const yData = data.map((d) => d[yProp]);
+	console.log(data);
+
+	let x = d3.scaleBand().domain(xData).range([0, width]).padding(0.2);
+
+	let y = d3
+		.scaleLinear()
+		.domain([d3.min(yData), d3.max(yData)])
+		.range([height, 0])
+		.nice(5);
+
+	let xAxis = d3
+		.axisBottom()
+		.scale(x)
+		.ticks(10)
+		.tickPadding(-x.bandwidth() / 2)
+		.tickSizeOuter(2)
+		.tickFormat((d) => d);
+
+	let yAxis = d3
+		.axisLeft()
+		.scale(y)
+		.ticks(5)
+		.tickFormat((d) => formatTimestamp(d));
+
+	let svg = d3
+		.select(containerID)
+		.append("svg")
+		.attr("width", width)
+		.attr("height", height)
+		.append("g")
+		.attr(
+			"transform",
+			"translate(" + 2 * style.left + "," + -style.bottom + ")"
+		);
+
+	svg.selectAll("bars")
+		.data(data)
+		.enter()
+		.append("rect")
+		.attr("x", (d) => {
+			return x(d.event);
+		})
+		.attr("y", (d) => {
+			return y(d.dur);
+		})
+		.attr("class", (d) => {
+			return d.class_name;
+		})
+		.attr("width", x.bandwidth())
+		.attr("height", (d) => {
+			console.log(height, d, y(d.dur));
+			return height - y(d.dur);
+		})
+		.attr("fill", (d) => {
+			return COLORS[d.class_name];
+		});
+
+	svg.append("g")
+		.attr("class", "x axis")
+		.attr("transform", "translate(0," + height + ")")
+		.call(xAxis)
+		.selectAll("text")
+		.attr("dy", x.bandwidth() / 2)
+		.attr("dx", -3)
+		.style("text-anchor", "end")
+		.attr("transform", "rotate(90)");
+
+	svg.append("g")
+		.attr("class", "y axis")
+		.call(yAxis)
+		.append("text")
+		.attr("transform", "rotate(-90)")
+		.attr("y", 0)
+		.attr("dy", "1em")
+		.style("text-anchor", "end")
+		.text("time (s))");
+}
+export default D3BarGraph;
