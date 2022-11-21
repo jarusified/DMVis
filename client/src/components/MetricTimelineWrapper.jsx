c808afc378d026d5077d38245092aadab3482089
jarusified <jarus3001@gmail.com>
2022-11-21 15:52:54 -0800

    Adjust styles, remove fit button

--- client/src/components/MetricTimelineWrapper.jsx
+++ client/src/components/MetricTimelineWrapper.jsx
@@ -35,7 +35,7 @@ function MetricTimelineWrapper() {
 	const style = {
 		top: 0,
 		right: 10,
-		bottom: 0,
+		bottom: 10,
 		left: 100,
 		width: window.innerWidth * 0.66 - 30,
 		height: 50
@@ -58,18 +58,13 @@ function MetricTimelineWrapper() {
 		const filtered_metrics = Object.keys(metricTimeline).filter((d) => d != "timestamp");
 		setFilteredMetrics(filtered_metrics);
 
-		// Interaction: Fit the timeline to the screenWidth.
-		document.getElementById("fit-button").onclick = function () {
-
-		};
-
 		// console.log("Fetching data for window: ", timelineStart, "-", timelineStart + sectorWidth);
 		// dispatch(fetchWindow(timelineStart, timelineStart + sectorWidth));
 	}, [metricTimeline]);
 
 	
 	return (
-		<Paper sx={{maxHeight: 160, overflow: 'auto' }}>
+		<Paper sx={{maxHeight: 200, overflow: 'auto' }}>
             <Grid item xs={4} p={1}>
 				<Typography
 					variant="overline"
@@ -93,7 +88,7 @@ function MetricTimelineWrapper() {
 								yData={metricTimeline[metric]}
 								xData={metricTimeline["timestamp"]}
 								style={style}
-								xProp={metric}
+								xProp={metric.slice(0, 20) + "..."}
 							/>
 					</Grid>
 				))}
