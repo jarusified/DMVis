2673d42cab5490edbfaede0c88b571e68b1d9da8
jarusified <jarus3001@gmail.com>
2022-11-24 00:29:43 -0800

    Fix metrics timeline view

--- client/src/components/MetricTimelineWrapper.jsx
+++ client/src/components/MetricTimelineWrapper.jsx
@@ -1,12 +1,11 @@
 import { useTheme } from "@emotion/react";
 import Grid from "@mui/material/Grid";
 import Paper from "@mui/material/Paper";
-
+import Typography from "@mui/material/Typography";
 import { makeStyles } from "@mui/styles";
 import * as d3 from "d3";
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import Typography from "@mui/material/Typography";
 
 import { fetchMetricTimeline } from "../actions";
 import D3LineGraph from "../ui/d3-line-graph";
@@ -18,7 +17,7 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 function MetricTimelineWrapper() {
-    const theme = useTheme();
+	const theme = useTheme();
 	const classes = useStyles();
 	const dispatch = useDispatch();
 
@@ -53,20 +52,24 @@ function MetricTimelineWrapper() {
 	}, [selectedExperiment, timelineStart, timelineEnd]);
 
 	useEffect(() => {
-		d3.select("#metric-timeline-view").selectAll("*").remove();
-		let container = document.getElementById("metric-timeline-view");
+		if (Object.keys(metricTimeline).length > 0) {
+			d3.select("#metric-timeline-view").selectAll("*").remove();
+			let container = document.getElementById("metric-timeline-view");
 
-		const filtered_metrics = Object.keys(metricTimeline).filter((d) => d != "timestamp");
-		setFilteredMetrics(filtered_metrics);
+			const filtered_metrics = Object.keys(metricTimeline).filter(
+				(d) => d != "timestamp"
+			);
 
-		// console.log("Fetching data for window: ", timelineStart, "-", timelineStart + sectorWidth);
-		// dispatch(fetchWindow(timelineStart, timelineStart + sectorWidth));
+			setFilteredMetrics(filtered_metrics);
+
+			// console.log("Fetching data for window: ", timelineStart, "-", timelineStart + sectorWidth);
+			// dispatch(fetchWindow(timelineStart, timelineStart + sectorWidth));
+		}
 	}, [metricTimeline]);
 
-	
 	return (
-		<Paper sx={{maxHeight: 250, overflow: 'auto' }}>
-            <Grid item xs={4} p={1}>
+		<Paper sx={{ maxHeight: 250, overflow: "auto" }}>
+			<Grid item xs={4} p={1}>
 				<Typography
 					variant="overline"
 					style={{
@@ -76,21 +79,21 @@ function MetricTimelineWrapper() {
 					}}
 				>
 					Metric Timeline {"  "}
-                    <span style={{ color: theme.text.label }}>
-                        ({Object.keys(metricTimeline).length} metrics)
-                    </span>
+					<span style={{ color: theme.text.label }}>
+						({Object.keys(metricTimeline).length} metrics)
+					</span>
 				</Typography>
 			</Grid>
-            <Grid container p={1}>
+			<Grid container p={1}>
 				{filteredMetrics.map((metric) => (
 					<Grid item key={metric}>
-							<D3LineGraph
-								containerName={metric + "timeline-view"}
-								yData={metricTimeline[metric]}
-								xData={metricTimeline["timestamp"]}
-								style={style}
-								xProp={metric}
-							/>
+						<D3LineGraph
+							containerName={metric + "timeline-view"}
+							yData={metricTimeline[metric]}
+							xData={metricTimeline["timestamp"]}
+							style={style}
+							xProp={metric}
+						/>
 					</Grid>
 				))}
 			</Grid>
