b9c3bb8160b01ffe9561d8bf8375e3e3a3a74046
Suraj Kesavan <jarus3001@gmail.com>
2024-01-16 01:30:28 -0800

    Initial attempt to fix device-device comm

--- client/src/components/DeviceDeviceCommunication.jsx
+++ client/src/components/DeviceDeviceCommunication.jsx
@@ -1,22 +1,20 @@
 import Card from "@mui/material/Card";
 import Grid from "@mui/material/Grid";
 import React, { useRef, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
 
-// import { fetchHostDeviceComm } from "../actions";
 import D3ChordDiagram from "../ui/d3-chord-diagram";
 import D3Matrix from "../ui/d3-matrix";
 
 export default function DeviceDeviceCommunication() {
 	const style = {
-		top: 10,
-		right: 40,
-		bottom: 10,
-		left: 20,
-		width: window.innerWidth / 3 - 30,
-		height: window.innerHeight / 3 - 20
+		top: 36,
+		right: 0,
+		bottom: 0,
+		left: 36,
+		width: window.innerWidth / 4,
+		height: window.innerHeight / 4
 	};
-	const containerID = useRef("host-device-view");
+	const containerID = useRef("device-device-view");
 
 	const [matrixType, setMatrixType] = useState("matrix");
 
@@ -24,9 +22,13 @@ export default function DeviceDeviceCommunication() {
 
 	// create a matrix
 	const matrixData = [
-		[0, 0, 8916, 2868],
-		[1951, 0, 2060, 6171]
+		[0, 0, 0, 0],
+		[0, 0, 0, 0],
+		[0, 0, 0, 0],
+		[0, 0, 0, 0],
 	];
+
+	console.log(matrixData);
 
 	// useEffect(() => {
 	// 	if (selectedExperiment !== "") {
@@ -38,20 +40,12 @@ export default function DeviceDeviceCommunication() {
 	return (
 		<Grid container>
 			{matrixData.length > 0 && matrixData[0].length > 0 ? (
-				<Card style={{ borderColor: "gray" }}>
-					{matrixType == "chord" ? (
-						<D3ChordDiagram
-							containerName={containerID.current}
-							style={style}
-							data={matrixData}
-						/>
-					) : (
-						<D3Matrix
-							containerName={containerID.current}
-							style={style}
-							data={matrixData}
-						/>
-					)}
+				<Card>
+					<D3Matrix
+						containerName={containerID.current}
+						style={style}
+						matrixDatata={matrixData}
+					/>
 				</Card>
 			) : (
 				<CircularProgress />
